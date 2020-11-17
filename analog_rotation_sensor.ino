4fd8e75f1b2805603bf07dbc87d918fdeff7af63
dannuke <davis.danielallen@gmail.com>
2020-11-16 17:31:40 -0700

    Update analog_rotation_sensor.ino

--- analog_rotation_sensor.ino
+++ analog_rotation_sensor.ino
@@ -1,11 +1,12 @@
 //notes by Dan Davis
 // introducing the const int code: declaration of a constant
-//equipment: Analog Rotation Sensor DFR 0058, Digital LED, Arduino, IDE serial monitor. 
+//equipment: Analog Rotation Sensor DFR 0058, Digital LED, Arduino, IDE serial monitor. Breadboard optional
 //direct connect to the Arduino
 //reference https://wiki.dfrobot.com/Analog_Rotation_Sensor_V2__SKU__DFR0058_
 //connect the rotational sensor leads to 5 volts, GND, and signal pin to analaog A0 on the Arduino 
 //connect the digital LED to 3.3 volts, GND,  and output to pin ~9
 //by rotating the  potentiometer, the LED will fade or get bright. Also, see the affect using the serial monitor
+// if a breadboard is used, connect the 5 volts and GND , for the sensor,  with leads to the breadboard rail, and the 3.3 volts  and GND for the LED to the otherside rails of the breaboard.
 
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 const int analogOutPin = 9; // Analog output pin that the LED is attached to
